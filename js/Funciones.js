fcd54a744a591e9a82bd188b45353b796b8b42ee
Mauro-Martinez-Cruz <113959572+Mauro-Martinez-Cruz@users.noreply.github.com>
2024-11-17 17:30:55 -0300

    Update

--- js/Funciones.js
+++ js/Funciones.js
@@ -82,8 +82,8 @@ function inicio() {
     /*\ |*| REGISTRO VISITA |*| \*/
 
     /*\ |*| SELECCIONAR OBRA TABLA |*| \*/
-    document.getElementById('idExpisicionesTabla').addEventListener('change', (e)=> {
-        if(sistema.getVisitas().length != 0){
+    document.getElementById('idExpisicionesTabla').addEventListener('change', (e) => {
+        if (sistema.getVisitas().length != 0) {
             let visitas = visitasSelecionadasTabla(e.target.value);
             ordenarCalificaciones(visitas);
         }
@@ -91,15 +91,15 @@ function inicio() {
     /*\ |*| SELECCIONAR OBRA TABLA |*| \*/
 
     /*\ |*| ORDEN CALIFICACION |*| \*/
-    document.getElementById('idBtnOrdenCalificacion').onclick = (e)=>{
-        if(sistema.getVisitas().length != 0){
+    document.getElementById('idBtnOrdenCalificacion').onclick = (e) => {
+        if (sistema.getVisitas().length != 0) {
             let visita = document.getElementById('idExpisicionesTabla').value;
             let visitas = visitasSelecionadasTabla(visita);
 
-            if(e.target.value == 'Calificación creciente'){
+            if (e.target.value == 'Calificación creciente') {
                 ordenarCalificaciones(visitas);
                 e.target.value = 'Calificación decreciente';
-            }else{
+            } else {
                 ordenarCalificaciones(visitas, "DECRECIENTE");
                 e.target.value = 'Calificación creciente';
             }
@@ -206,6 +206,9 @@ function cargarLista(list) {
     document.getElementById('idListaArtistasDos').innerHTML = ``;
     let selectListArtistas = document.getElementById('idListaArtistasUno');
     selectListArtistas.innerHTML = ``;
+
+    list.sort((a, b) => a.getNombre().localeCompare(b.getNombre())); // "localeCompare" -> METODO DE TIPO STRING PARA COMPARAR
+
     list.forEach(artista => {
         let opt = document.createElement("option");
         opt.value = artista.getNombre();
@@ -275,11 +278,11 @@ function pasajeArtistasListas(listaA, listaB, select = true) {
 
     for (let i = 0; i < listaA.options.length; i++) {
         const opt = listaA.options[i];
-        if(select){
+        if (select) {
             if (opt.selected) {
                 seleccionados.push(opt);
             }
-        }else{
+        } else {
             seleccionados.push(opt)
         }
     }
@@ -355,6 +358,8 @@ function registroExposicion() {
         let exposicion = new Exposicion(titulo.value, fecha.value, descripcion.value, arrArtistas);
         sistema.addExposicion(exposicion);
         exposicionesEnVisitas();
+        cargarListaExposicionesConMasArtistas(exposicionConMasArtistas());
+        cargarListaExposicionesSinComentarios(exposicionesSinComentarios());
 
         titulo.value = "";
         fecha.value = "";
@@ -369,7 +374,7 @@ function registroExposicion() {
 
 /*================== EXPOSICIÓN EN VISITAS (INICIO) =================*/
 
-function optionExposicion(exposicion){
+function optionExposicion(exposicion) {
     let option = document.createElement('OPTION');
     option.textContent = exposicion.getTitulo();
     option.value = sistema.getExposiciones().indexOf(exposicion);
@@ -379,7 +384,7 @@ function optionExposicion(exposicion){
 function exposicionesEnVisitas() {
     let exposiciones = document.getElementById('idVisitaExpo');
     let expoTabla = document.getElementById('idExpisicionesTabla');
-    
+
     exposiciones.innerHTML = '';
     expoTabla.innerHTML = `
         <option value="all">Todas</option>
@@ -441,6 +446,7 @@ function registroVisita() {
         let visita = new Visita(sistema.getExposiciones()[exposicion.value], nombre.value, comentario.value, calificacion, guiada);
         sistema.addVisita(visita);
         agregarComentario();
+        cargarListaExposicionesSinComentarios(exposicionesSinComentarios());
 
         nombre.value = "";
         comentario.value = "";
@@ -455,7 +461,7 @@ function registroVisita() {
 
 /*================== COMENTARIOS EN TABLA (INICIO) =================*/
 
-function agregarComentario(orden = sistema.getVisitas()){
+function agregarComentario(orden = sistema.getVisitas()) {
     let body = document.getElementById('idTablaComentarios');
     body.innerHTML = '';
 
@@ -488,13 +494,13 @@ function agregarComentario(orden = sistema.getVisitas()){
 
 function formatoFecha(fecha) {
     let date = new Date(fecha);
-    let dia = date.getDate(); 
+    let dia = date.getDate() + 1;
     let mes = date.getMonth() + 1;
-    let año = date.getFullYear();
+    let anio = date.getFullYear();
 
     dia = (dia < 10) ? '0' + dia : dia; // ES UN IF DE UNA LINEA "(CONDICION)" "?" -> SI PASA LA CONDICIÓN ":" -> SINO
     mes = (mes < 10) ? '0' + mes : mes;
-    return dia + '/' + mes + '/' + año;
+    return dia + '/' + mes + '/' + anio;
 }
 
 /*================== FORMATEAR LA FECHA (FINAL) =================*/
@@ -504,20 +510,20 @@ function formatoFecha(fecha) {
 /*================== INFORMACIÓN DE EXPOSICIÓN (INICIO) =================*/
 
 function ampliarInformacionObras() {
-        let btnsAmpliar = document.querySelectorAll('.table__btn__ampliar')
-        btnsAmpliar.forEach(btn => {
-            btn.onclick = () => {
-                let index = btn.dataset.expo;
-                let exposicion = sistema.getVisitas()[index].getExposicion();
-                let msg = `
+    let btnsAmpliar = document.querySelectorAll('.table__btn__ampliar')
+    btnsAmpliar.forEach(btn => {
+        btn.onclick = () => {
+            let index = btn.dataset.expo;
+            let exposicion = sistema.getVisitas()[index].getExposicion();
+            let msg = `
                     <p class="modal__texto"><span class="modal__texto--span">Fecha: </span>${formatoFecha(exposicion.getFecha())}</p>
                     <p class="modal__texto"><span class="modal__texto--span">Descripción: </span>${exposicion.getDescripcion()}</p>
                     <p class="modal__texto"><span class="modal__texto--span">Artistas:</span></p>
                 `;
-                msg += tablaArtistasExpocision(exposicion);
-                modal('Información de la Exposición', msg);
-            };
-        });
+            msg += tablaArtistasExpocision(exposicion);
+            modal('Información de la Exposición', msg);
+        };
+    });
 }
 
 function tablaArtistasExpocision(exposicion) {
@@ -559,10 +565,10 @@ function ordenarCalificaciones(visitas, direccion = 'CRECIENTE') {
     const orden = ['caraMuyMal', 'caraMal', 'caraNormal', 'caraBien', 'caraMuyBien'];
     let retorno = [];
 
-    if(direccion == 'CRECIENTE'){
-       retorno = visitas.sort((a, b) => orden.indexOf(a.calificacion) - orden.indexOf(b.calificacion));
-    }else{
-       retorno = visitas.sort((a, b) => orden.indexOf(b.calificacion) - orden.indexOf(a.calificacion));
+    if (direccion == 'CRECIENTE') {
+        retorno = visitas.sort((a, b) => orden.indexOf(a.calificacion) - orden.indexOf(b.calificacion));
+    } else {
+        retorno = visitas.sort((a, b) => orden.indexOf(b.calificacion) - orden.indexOf(a.calificacion));
     }
     agregarComentario(retorno);
 }
@@ -576,12 +582,12 @@ function ordenarCalificaciones(visitas, direccion = 'CRECIENTE') {
 function visitasSelecionadasTabla(seleccionadas) {
     let select = [];
 
-    if(seleccionadas == "all"){
+    if (seleccionadas == "all") {
         select = sistema.getVisitas();
-    }else{
+    } else {
         console.log(sistema.getExposiciones()[seleccionadas]);
         sistema.getVisitas().forEach(visita => {
-            if(visita.getExposicion() == sistema.getExposiciones()[seleccionadas]){
+            if (visita.getExposicion() == sistema.getExposiciones()[seleccionadas]) {
                 select.push(visita);
             }
         });
@@ -592,3 +598,91 @@ function visitasSelecionadasTabla(seleccionadas) {
 
 /*================== SOLO VISITAS SELECIONADAS (FINAL) =================*/
 
+/*===============================================================*/
+
+/*================== EXPOSICION CON MAS ARTISTAS (INICIO) =================*/
+
+function exposicionConMasArtistas() {
+    let maximo = 0;
+    let artistasMaximos = [];
+
+    sistema.getExposiciones().forEach(exposicion => {
+        if (exposicion.getArtistas().length > maximo) {
+            if (artistasMaximos.length > 0) { artistasMaximos = []; }
+            maximo = exposicion.getArtistas().length;
+            artistasMaximos.push(exposicion);
+        } else if (exposicion.getArtistas().length == maximo) {
+            artistasMaximos.push(exposicion);
+        }
+    });
+
+    return artistasMaximos;
+}
+
+/*================== EXPOSICION CON MAS ARTISTAS (FINAL) =================*/
+
+/*===============================================================*/
+
+/*================== CARGAR LISTA EXPOSICIONES CON MAS ARTISTAS (INICIO) =================*/
+
+function cargarListaExposicionesConMasArtistas(exposiciones) {
+    let ul = document.getElementById('idListaExposicionesMasArtistas');
+    ul.innerHTML = ``;
+
+    exposiciones.forEach(exposicion => {
+        let li = document.createElement('LI');
+        li.textContent = `${exposicion.getTitulo()}`;
+        ul.appendChild(li);
+    });
+}
+
+/*================== CARGAR LISTA EXPOSICIONES CON MAS ARTISTAS (FINAL) =================*/
+
+/*===============================================================*/
+
+/*================== EXPOSICION SIN COMENTARIOS (INICIO) =================*/
+
+function exposicionesSinComentarios() {
+    let conVisitas = [];
+    let expSinVisitas = [];
+
+    sistema.getVisitas().forEach(visita => {
+        if (!conVisitas.includes(visita.getExposicion())) {
+            conVisitas.push(visita.getExposicion());
+        }
+    });
+
+    sistema.getExposiciones().forEach(exposicion => {
+        if (!conVisitas.includes(exposicion)) {
+            expSinVisitas.push(exposicion);
+        }
+    });
+
+    expSinVisitas.sort((a, b) => new Date(a.getFecha()) - new Date(b.getFecha()));
+
+    return expSinVisitas;
+}
+
+/*================== EXPOSICION SIN COMENTARIOS (FINAL) =================*/
+
+/*===============================================================*/
+
+/*================== CARGAR LISTA EXPOSICIONES SIN COMENTARIO (INICIO) =================*/
+
+function cargarListaExposicionesSinComentarios(exposiciones) {
+    let ul = document.getElementById('idListaExposicionesSinComentarios');
+    ul.innerHTML = ``;
+
+    if (exposiciones.length > 0) {
+        exposiciones.forEach(exposicion => {
+            let li = document.createElement('LI');
+            li.textContent = `${exposicion.getTitulo()}   ${formatoFecha(exposicion.getFecha())}`;
+            ul.appendChild(li);
+        });
+    } else {
+        ul.innerHTML = "<li>Sin datos</li>";
+    }
+
+}
+
+/*================== CARGAR LISTA EXPOSICIONES SIN COMENTARIO (FINAL) =================*/
